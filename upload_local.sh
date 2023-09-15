8b7399087f2a18f1f12290613afba1b48d1b6306
Brookzerker <brooks@brookzerker.com>
2023-09-15 06:36:04 -0600

    adding upload script and ignoring log file

---
+++ upload_local.sh
@@ -0,0 +1,45 @@
+#!/bin/zsh
+
+# We have updated the local version of our .zshrc file, so we need to
+# - overwrite the one here
+# - upload to github
+LOGFILE=script_out
+
+echo "\n***** Running upload to github script *****\n" >> $LOGFILE
+
+if [[ -f $HOME/.zshrc ]]; then
+  echo "found .zshrc file in home directory"
+else
+  echo "could not find .zshrc file in home directory"
+  exit 1
+fi
+
+if git pull >> $LOGFILE; then
+  echo "updated .zshrc from github"
+else
+  echo "failed to update .zshrc from github"
+  exit 2
+fi
+
+if cp $HOME/.zshrc ./.zshrc >> $LOGFILE; then
+  echo "copied in .zshrc"
+else
+  echo "failed to copy in .zshrc from home directory"
+  exit 3
+fi
+
+if git commit -am "updating .zshrc from local" >> $LOGFILE; then
+  echo "commited to git"
+else
+  echo "failed to commit new version of .zshrc file"
+  exit 4
+fi
+
+if git push >> $LOGFILE; then
+  echo "pushed to github"
+else
+  echo "failed to push changes to github"
+  exit 5
+fi
+
+echo "local .zshrc pushed to github"
